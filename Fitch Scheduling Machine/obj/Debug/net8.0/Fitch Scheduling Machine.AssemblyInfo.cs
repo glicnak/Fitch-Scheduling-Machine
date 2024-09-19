e37adf2db360137127fdecd683d1f123202b419e
glicnak <adamlisiverrillo@gmail.com>
2024-09-19 15:33:52 -0400

    Recursion Works

--- Fitch Scheduling Machine/obj/Debug/net8.0/Fitch Scheduling Machine.AssemblyInfo.cs
+++ Fitch Scheduling Machine/obj/Debug/net8.0/Fitch Scheduling Machine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98534a457100ed947bb78824fe6befc4041e06ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4343c5d6d26ed4e1ca66fb0228eaa83a728cfaba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
