4d55b7ae32f9d9e74df51cf03b2e48f465cac8fc
glicnak <adamlisiverrillo@gmail.com>
2024-09-22 10:07:16 -0400

    Mostly works, just one problem

--- Fitch Scheduling Machine/ForcedCourses.cs
+++ Fitch Scheduling Machine/ForcedCourses.cs
@@ -24,7 +24,15 @@ namespace Fitch_Scheduling_Machine
                 {allCourses.Find(c => c.courseName == "Out Ed Ex D"), new List<int[]> {new int[] {2,1}, new int[] {2,2}}},
                 {allCourses.Find(c => c.courseName == "Out Ed Ex E"), new List<int[]> {new int[] {3,1}, new int[] {3,2}}},
                 {allCourses.Find(c => c.courseName == "Out Ed Ex F"), new List<int[]> {new int[] {3,1}, new int[] {3,2}}},
-                {allCourses.Find(c => c.courseName == "Out Ed Ex G"), new List<int[]> {new int[] {3,1}, new int[] {3,2}}}
+                {allCourses.Find(c => c.courseName == "Out Ed Ex G"), new List<int[]> {new int[] {3,1}, new int[] {3,2}}},
+
+                {allCourses.Find(c => c.courseName == "Work1 WOTP AM"), new List<int[]> {new int[] {0,3}, new int[] {2,3}}},
+                {allCourses.Find(c => c.courseName == "Work2 WOTP AM"), new List<int[]> {new int[] {0,4}, new int[] {1,4}, new int[] {2,4}, new int[] {3,4}, new int[] {4,4}}},
+                {allCourses.Find(c => c.courseName == "Work3 WOTP AM"), new List<int[]> {new int[] {0,5}, new int[] {1,5}, new int[] {2,5}, new int[] {3,5}, new int[] {4,5}}},
+
+                {allCourses.Find(c => c.courseName == "Work1 WOTP PM"), new List<int[]> {new int[] {0,0}, new int[] {1,0}, new int[] {2,0}, new int[] {3,0}, new int[] {4,0}}},
+                {allCourses.Find(c => c.courseName == "Work2 WOTP PM"), new List<int[]> {new int[] {0,1}, new int[] {1,1}, new int[] {2,1}, new int[] {3,1}, new int[] {4,1}}},
+                {allCourses.Find(c => c.courseName == "Work3 WOTP PM"), new List<int[]> {new int[] {1,2}, new int[] {3,2}}}
             };
             return forcedCourses;
         }
--- Fitch Scheduling Machine/obj/Debug/net8.0/Fitch Scheduling Machine.AssemblyInfo.cs
+++ Fitch Scheduling Machine/obj/Debug/net8.0/Fitch Scheduling Machine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9935f09ef73095f85ef26c6a5eda2e5c335640")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb54d7c7d620013a3b8de0a27faecfa2ec3d58a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
