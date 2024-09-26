3c547fcc2950db6f28f6e95b391ad0ff64351820
glicnak <adamlisiverrillo@gmail.com>
2024-09-26 18:04:36 -0400

    Fixed More

--- Fitch Scheduling Machine/obj/Debug/net8.0/Fitch Scheduling Machine.AssemblyInfo.cs
+++ Fitch Scheduling Machine/obj/Debug/net8.0/Fitch Scheduling Machine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79dbce3b7e2fce61e431644dbd0fb2abb69c282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94995cc5bfdefcd57194d2e45437a9083f9a41ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fitch Scheduling Machine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
